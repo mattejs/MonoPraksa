029909417cbfe4c6cbebde959759e405b8d8779a
Matej <msarcevic997@gmail.com>
2022-01-25 15:37:00 +0100

    Day 2 - WebAPI

---
+++ Day2/StudentsWebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,89 @@
+﻿using StudentsWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentsWebAPI.Controllers
+{
+    public class StudentsController : ApiController
+    {             
+
+        // GET api/students
+        public HttpResponseMessage Get()
+        {
+            if(StudentsList.Read().Count == 0) 
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, "No data");
+            }
+            else if(StudentsList.Read() != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, StudentsList.Read());
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Error");
+            }
+        }
+
+        // GET api/students/5
+        public HttpResponseMessage GetById(int id)
+        {
+            if(StudentsList.GetById(id) != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, StudentsList.GetById(id));
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There is no student with ID: " + id);
+            }            
+        }
+
+
+        // POST api/students
+        public HttpResponseMessage Post([FromBody] Student student)
+        {
+            try
+            {
+                StudentsList.Create(student);
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Error");
+            }
+        }
+
+        // PUT api/students/5
+        public HttpResponseMessage Put(int id, [FromBody] Student student)
+        {
+            if (StudentsList.GetById(id) != null)
+            {
+                StudentsList.Update(id, student);
+                return Request.CreateResponse(HttpStatusCode.NoContent);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Can't update, there is no student with ID: " + id);
+            }
+
+        }
+
+        // DELETE api/students/5
+        public HttpResponseMessage Delete(int id)
+        {
+            if (StudentsList.GetById(id) != null)
+            {
+                StudentsList.Delete(id);
+                return Request.CreateResponse(HttpStatusCode.NoContent);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Can't delete, there is no student with ID: " + id);
+            }
+        }
+    }
+
+}
---
+++ Day2/StudentsWebAPI/Models/StudentsList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentsWebAPI.Models
+{
+    public static class StudentsList
+    {
+        static List<Student> students = new List<Student>();
+        static int nextId = 1;
+
+        public static Student GetById(int id) => students.FirstOrDefault(p => p.Id == id);
+
+        public static void Create(Student student)
+        {
+            student.Id = nextId++;
+            students.Add(student);
+        }
+
+        public static List<Student> Read()
+        {
+            return students;
+        }
+        public static void Update(int id, Student student)
+        {
+            var index = new Student();
+            index= students.Where(p => p.Id == id).FirstOrDefault();
+            index.FirstName = student.FirstName;
+            index.LastName = student.LastName;
+        }
+
+        public static void Delete(int id)
+        {            
+            var student = GetById(id);
+            students.Remove(student);            
+        }
+
+    }
+}
